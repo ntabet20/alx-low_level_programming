717a888c04847467569a8bf65980136ab9e04598
Salomon Pierre <ntabetsalomon@gmail.com>
2023-03-16 12:50:30 +0000

    Adding 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
 
+/**
+ * main - All instructions executed here
+ *
+ * Return 0 : Always gives 0
+ */
+
 int main(void){
 	 
-	int puts("\"Programming is like building a multilingual puzzle");
+        puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
