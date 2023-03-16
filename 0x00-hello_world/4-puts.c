14c04e57a4e75aa595fa8505243c77b5edc5bba4
Salomon Pierre <ntabetsalomon@gmail.com>
2023-03-16 12:56:58 +0000

    Adding 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 /**
  * main - All instructions executed here
  *
- * Return 0 : Always gives 0
+ * Return: Always gives 0
  */
 int main(void)
 {	 
-        puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
