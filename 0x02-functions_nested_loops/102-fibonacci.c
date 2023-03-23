58d91500857d36564eca6fce0c8be49e52a33ae4
Salomon Pierre <ntabetsalomon@gmail.com>
2023-03-23 13:15:47 +0000

    Adding 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,16 +8,16 @@
 int main(void)
 {
 	long int n1 = 1, n2 = 2, fib;
-	int i;
+	int i = 1;
 
 	printf("%ld, %ld, ", n1, n2);
-	while (i <= 52)
+	while (i <= 48)
 	{
 		fib = n1 + n2;
 		printf("%ld", fib);
 		n1 = n2;
 		n2 = fib;
-		if (i == 50)
+		if (i == 48)
 			break;
 		printf(", ");
 		i++;
