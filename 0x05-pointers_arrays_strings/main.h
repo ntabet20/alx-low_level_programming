e2544c9e57eae9ff060ced22c182a7dc4131b7c6
Salomon Pierre <ntabetsalomon@gmail.com>
2023-03-28 09:46:48 +0000

    Adding 8-print_array.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -5,3 +5,8 @@ void swap_int(int *, int *);
 int _strlen(char *);
 void _puts(char *);
 void print_rev(char *);
+void rev_string(char *);
+void puts2(char *);
+void puts_half(char *);
+void print_array(int *, int);
+char *_strcpy(char *dest, char *src);
