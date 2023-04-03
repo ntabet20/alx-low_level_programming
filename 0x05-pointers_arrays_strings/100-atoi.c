5075ef6d929a5ff8a31f8ab270936c4fbd4ecce7
Salomon Pierre <ntabetsalomon@gmail.com>
2023-04-03 22:12:43 +0000

    Updated repository

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _atoi - converts the initial portion of the string pointed by
+           s to an integer.
+ * @s: the string itself
+ *
+ */
+int _atoi(char *s)
+{
+
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,3 @@
+/* Functions prototypes to manipulate pointers, arrays and strings */
+char *_strcat(char *, char*);
+int _putchar(char);
